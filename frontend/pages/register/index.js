9028119565ee04749db6420260c660d339cce49e
lirc572 <lirc572@gmail.com>
2021-05-14 02:39:20 +0800

    feat: update login and register page

--- frontend/pages/register/index.js
+++ frontend/pages/register/index.js
@@ -1,3 +1,104 @@
+import { Form, Input, Button, Checkbox } from "antd";
+import { useRouter } from "next/router";
+import axios from "axios";
+
+async function registerAccount(username, password) {
+  try {
+    const res = await axios.post("http://localhost:5000/api/v1/register", {
+      username,
+      password,
+    });
+    return res;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        return error.response;
+      }
+    } else {
+      console.log(error);
+    }
+  }
+}
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
 export default function RegisterPage() {
-    return <h1>This is where you register an account...</h1>
+  const router = useRouter();
+
+  const onLoginButtonClick = () => {
+    router.push("/login");
+  };
+
+  const onFinish = (values) => {
+    registerAccount(values.username, values.password).then((res) => {
+      if (!res) {
+        console.log("Stupid error");
+        return;
+      }
+      if (res.status == 204) {
+        router.push("/login");
+      } else {
+        console.log(res);
+      }
+    });
+  };
+
+  const onFinishFailed = (errorInfo) => {};
+
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        <Button type="link" htmlType="button" onClick={onLoginButtonClick}>
+          Login
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
