40cd133b61d42234a7cf3aff72a059a275d1a22d
lirc572 <lirc572@gmail.com>
2021-05-15 04:14:32 +0800

    feat: add dashboard page layout

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -1,3 +1,73 @@
+import { useEffect, useState } from "react";
+import { Row, Col, Divider, Button, List, Space } from "antd";
+import styles from "../../styles/Dashboard.module.css";
+import { readShortUrls } from "../../util/api";
+import { useRouter } from "next/router";
+
+const data = [
+  {
+    alias: "tmp",
+    url: "https://abc.com",
+  },
+  {
+    alias: "bbc",
+    url: "https://stupid.io",
+  },
+  {
+    alias: "google",
+    url: "https://google.com",
+  },
+  {
+    alias: "git",
+    url: "https://github.com",
+  },
+];
+
 export default function DashboardPage() {
-  return <h1>Here is the dashboard page...</h1>;
+  const [shortUrls, setShortUrls] = useState([]);
+  useEffect(async () => {
+    const data = await readShortUrls();
+    if (data) {
+      setShortUrls(data);
+    }
+  });
+  return (
+    <div className={styles.main}>
+      <Row justify="center">
+        <Col span={20} className={styles.column}>
+          <div className={styles.headerdivider}>
+            <span className={styles.heading}>Aliases</span>
+            <Button type="primary" ghost>
+              Add New
+            </Button>
+          </div>
+          <Divider />
+          <div className={styles.fullgrow}>
+            <List
+              dataSource={shortUrls}
+              renderItem={(item) => (
+                <List.Item>
+                  <Row className={styles.entryrow}>
+                    <Col span={4}>{item.alias}</Col>
+                    <Col span={16}>{item.url}</Col>
+                    <Col span={4}>
+                      <Space size="middle">
+                        <Button shape="round" type="primary">
+                          Edit
+                        </Button>
+                        <Button shape="round" type="primary" danger>
+                          Delete
+                        </Button>
+                      </Space>
+                    </Col>
+                  </Row>
+                </List.Item>
+              )}
+            />
+          </div>
+        </Col>
+      </Row>
+      <div className={styles.fullgrow} />
+    </div>
+  );
 }
--- frontend/util/api.js
+++ frontend/util/api.js
@@ -1,12 +1,78 @@
 import { initializeStore } from "../store";
+import { message } from "antd";
 import axios from "axios";
 
+let instance;
+
+function initInstance() {
+  const { baseUrl, accessToken } = initializeStore().getState();
+  return axios.create({
+    baseURL: baseUrl,
+    timeout: 10000,
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+}
+
+function getAxiosInstance() {
+  let _instance = instance ?? initInstance();
+
+  // Use the latest working access token
+  _instance.interceptors.request.use(
+    function (config) {
+      const { accessToken } = initializeStore().getState();
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+      return config;
+    },
+    function (error) {
+      console.log(`Set token error: ${error}`);
+      return Promise.reject(error);
+    }
+  );
+
+  // Automatically update access token if the server issues one in the response headers
+  _instance.interceptors.response.use(
+    function (response) {
+      if (response.headers["Updated-Token"]) {
+        initializeStore().dispatch({
+          type: "SET_ACCESS_TOKEN",
+          payload: {
+            accessToken: response.headers["Updated-Token"],
+          },
+        });
+      }
+      return response;
+    },
+    function (error) {
+      if (error.response.status === 401) {
+        message.error("Authentication failed. Please login again!", 5);
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  if (!instance) {
+    instance = _instance;
+  }
+  return _instance;
+}
+
 export async function registerAccount(username, password) {
-  const { baseUrl } = initializeStore().getState();
+  const _instance = getAxiosInstance();
   try {
-    const res = await axios.post(`${baseUrl}/register`, {
-      username,
-      password,
+    const res = await _instance({
+      method: "post",
+      url: "/register",
+      transformRequest: (data, headers) => {
+        delete headers["Authorization"];
+        headers["Content-Type"] = "application/json";
+        return JSON.stringify(data);
+      },
+      data: {
+        username,
+        password,
+      },
     });
     return res;
   } catch (error) {
@@ -21,11 +87,20 @@ export async function registerAccount(username, password) {
 }
 
 export async function loginToAccount(username, password) {
-  const { baseUrl } = initializeStore().getState();
+  const _instance = getAxiosInstance();
   try {
-    const res = await axios.post(`${baseUrl}/login`, {
-      username,
-      password,
+    const res = await _instance({
+      method: "post",
+      url: "/login",
+      transformRequest: (data, headers) => {
+        delete headers["Authorization"];
+        headers["Content-Type"] = "application/json";
+        return JSON.stringify(data);
+      },
+      data: {
+        username,
+        password,
+      },
     });
     return res;
   } catch (error) {
@@ -36,5 +111,110 @@ export async function loginToAccount(username, password) {
     } else {
       console.log(error);
     }
+  }
+}
+
+export async function checkToken() {
+  const _instance = getAxiosInstance();
+  try {
+    const res = await _instance({
+      method: "post",
+      url: "/checktoken",
+    });
+    return true;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (
+        (error.response && error.response.status > 299) ||
+        error.response.status < 200
+      ) {
+        return false;
+      }
+    }
+    return null;
+  }
+}
+
+export async function readShortUrls() {
+  const _instance = getAxiosInstance();
+  try {
+    const res = await _instance({
+      method: "get",
+      url: "/api/v1/shorturls",
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function createShortUrl(alias, url) {
+  const _instance = getAxiosInstance();
+  try {
+    const res = await _instance({
+      method: "post",
+      url: "/api/v1/shorturls",
+      data: {
+        alias,
+        url,
+      },
+    });
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+export async function readShortUrl(alias) {
+  const _instance = getAxiosInstance();
+  try {
+    const res = await _instance({
+      method: "get",
+      url: "/api/v1/shorturls",
+      params: {
+        alias,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateShortUrl(alias, url) {
+  const _instance = getAxiosInstance();
+  try {
+    const res = await _instance({
+      method: "put",
+      url: "/api/v1/shorturls",
+      params: {
+        alias,
+      },
+      data: {
+        url,
+      },
+    });
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+export async function deleteShortUrl(alias) {
+  const _instance = getAxiosInstance();
+  try {
+    const res = await _instance({
+      method: "delete",
+      url: "/api/v1/shorturls",
+      params: {
+        alias,
+      },
+    });
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
   }
 }
